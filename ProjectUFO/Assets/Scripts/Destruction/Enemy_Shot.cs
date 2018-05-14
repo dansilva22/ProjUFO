5c78aaac79d5380b599cf8b84340f0dbad4dafbb
Dan <ds3285@drexel.edu>
2018-05-13 22:14:34 -0400

    Explosion stuff edits

--- ProjectUFO/Assets/Scripts/Destruction/Enemy_Shot.cs
+++ ProjectUFO/Assets/Scripts/Destruction/Enemy_Shot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy_Shot : MonoBehaviour {
+    public GameObject prefab;
 
 	public void GotShot() {
 
@@ -10,11 +11,15 @@ public class Enemy_Shot : MonoBehaviour {
 	}
 
 	private IEnumerator Die() {
-		this.transform.Rotate (-75, 0, 0);
+		this.transform.Rotate (0, 0, 0);
 
-		yield return new WaitForSeconds (1.5f);
+		//yield return new WaitForSeconds (1.5f);
+        GameObject explosion;
 
+        explosion = Instantiate(prefab, transform.position, Quaternion.identity);
 		Destroy (this.gameObject);
+
+        yield return null; 
 	}
 
 }
