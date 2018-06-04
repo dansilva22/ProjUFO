49b98cb99bc1ece0201f2b1b526e24d8bbea2007
MSI\trans\Brandon Petroski <soundwave115@gmail.com>
2018-06-03 20:45:42 -0400

    collectme script goes on brain collectable prefab. this allows it to fly to whatever player is tagged "UFO" Enemy_Shot script has line to spawn brains depending on score UFO_Player prefab has UI on dash now. Please put in Jordans ttf font.

---
+++ ProjectUFO/Assets/collectMe.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectMe : MonoBehaviour {
+
+    
+    public float flyTime = 1f;
+
+    public GameObject flyToo;
+
+	// Use this for initialization
+	void Start () {
+
+        flyToo = GameObject.FindWithTag("UFO");
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        
+        transform.position = Vector3.Lerp(transform.position, flyToo.transform.position, flyTime * Time.deltaTime);
+
+
+
+	}
+
+    public void OnCollisionEnter(Collision node)
+    {
+        if (node.gameObject.tag == "UFO")
+        {
+            Destroy(node.gameObject);
+        }
+    }
+}
